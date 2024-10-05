c8788db1556e24d152bd0a28f4d2d0facbfb5f0f
yeojunh <yeojunhann@gmail.com>
2024-10-05 15:06:49 -0700

    Modify endpoints with dummy messages

--- backend/main.py
+++ backend/main.py
@@ -11,35 +11,68 @@ socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")  # Socket
 def index():
     return jsonify({"status": "OK"})
 
-@app.route("/hello", methods=['GET'])
-def send_message_to_client():
-    socketio.emit('hello', "Hello from the server!")
-    return jsonify({"hello": "Hello from the server!"})
+# always sends ALL data, assume nothing is persisted in the front end
 
-@app.route("/sink", methods=['GET'])
-def send_sink_to_client(): 
-    socketio.emit('sink', 'insert image url here hehe')
-    return jsonify({"sink": "insert image url here hehe"})
+# logs ordered by who is doing what, independent by time
+# for the dashboard and leaderboard
+# per user grouping 
+@app.route("/activity", methods=['GET'])
+def send_activity_to_client():
+    message = [] # dummy data
+    socketio.emit('activity', message)
+    return jsonify({"activity": "insert picture of new offender here"})
 
-@app.route("/offender", methods=['GET'])
-def send_offender_to_client():
-    socketio.emit('offender', 'insert picture of new offender here')
-    return jsonify({"offender": "insert picture of new offender here"})
+# log 
+# for main feed "recent logs"
+@app.route("/log", methods=['GET'])
+def send_log_to_client():
+    message = [ # dummy data
+        {
+            id: 1, 
+            name: "Leo Ciappi",
+            logs: [
+                {
+                    id: "event",
+                    image: "https://as2.ftcdn.net/v2/jpg/01/75/93/51/1000_F_175935137_aPD2ZOgBiey7Tlqz5PTXPqtmJnX9ZYU0.jpg",
+                    time: "1:32 PM",
+                    event: "Cleaned"
+                },
+                {
+                    id: "event",
+                    image: "https://as2.ftcdn.net/v2/jpg/01/75/93/51/1000_F_175935137_aPD2ZOgBiey7Tlqz5PTXPqtmJnX9ZYU0.jpg",
+                    time: "2:00 AM",
+                    event: "Contaminated"
+                },
+            ],
+        },
+        {
+            id: "2",
+            name: "Yeojun Han",
+            logs: [
+                {
+                    id: "event",
+                    image: "https://as2.ftcdn.net/v2/jpg/01/75/93/51/1000_F_175935137_aPD2ZOgBiey7Tlqz5PTXPqtmJnX9ZYU0.jpg",
+                    time: "1:54 AM",
+                    event: "Contaminated"
+                },
+            ],
+        }
+    ]
+    socketio.emit('log', message)
+    return jsonify({"log": "insert picture of new offender here"})
 
-@app.route("/offenders", methods=['GET'])
-def send_offenders_to_client():
-    socketio.emit('offenders', 'insert pictures of offenders here')
-    return jsonify({"offenders": "insert pictures of offenders here"})
-
-@app.route("/notification", methods=['GET'])
-def send_notification_to_client():
-    socketio.emit('notification', 'insert new notification here')
-    return jsonify({"notification": "insert new notification here"})
-
-@app.route("/notifications", methods=['GET'])
-def send_notifications_to_client():
-    socketio.emit('notifications', 'insert list of notifications here')
-    return jsonify({"notifications": "insert list of notifications here"})
+# notifications for given user
+@app.route("/notifications/<username>", methods=['GET'])
+def send_notifications_to_client(username):
+    # Dummy data for demonstration
+    # notifications = {
+    #     "Leo Ciappi": ["Notification 1", "Notification 2"],
+    #     "Yeojun Han": ["Notification 3"]
+    # }
+    # user_notifications = notifications.get(username, [])
+    message = [] # dummy data
+    socketio.emit('notifications', )
+    return jsonify({"notifications": user_notifications})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=8080, host='0.0.0.0')
